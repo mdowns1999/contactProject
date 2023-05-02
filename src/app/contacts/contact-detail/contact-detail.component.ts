59040726bff0127cc0d905503414eb504f377b44
mdowns <michaeljdowns99@gmail.com>
2023-05-02 15:17:59 -0600

    Worked on Assignment and Fixed HTML to components

--- src/app/contacts/contact-detail/contact-detail.component.ts
+++ src/app/contacts/contact-detail/contact-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Contact } from '../contact.model';
 
 @Component({
@@ -8,5 +8,6 @@ import { Contact } from '../contact.model';
 })
 export class ContactDetailComponent {
 
-  contact = new Contact("1", "Name Here", "Email Here", "555-5555", "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg", null);
+  @Input() contact: Contact;
+  //contact = new Contact("1", "Name Here", "Email Here", "555-5555", "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg", null);
 }
--- src/app/contacts/contact-item/contact-item.component.ts
+++ src/app/contacts/contact-item/contact-item.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Contact } from '../contact.model';
 
 @Component({
   selector: 'cms-contact-item',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class ContactItemComponent {
 
+@Input() contact: Contact;
 }
