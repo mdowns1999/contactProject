dacaae9475735149fc1274a5e1d122b332b01b28
mdowns <michaeljdowns99@gmail.com>
2023-05-02 11:11:01 -0600

    Fixed Contact No Strict

---
+++ src/app/contacts/contact-detail/contact-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Contact } from '../contact.model';
+
+@Component({
+  selector: 'cms-contact-detail',
+  templateUrl: './contact-detail.component.html',
+  styleUrls: ['./contact-detail.component.css']
+})
+export class ContactDetailComponent {
+
+  contact = new Contact("1", "Name Here", "Email Here", "555-5555", "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg", null);
+}
---
+++ src/app/contacts/contact.model.ts
@@ -0,0 +1,14 @@
+export class Contact{
+
+
+    // Contact[]
+    constructor(public id: string, 
+        public name: string, 
+        public email: string, 
+        public phone: string, 
+        public imageUrl: string, 
+        public group: any){
+
+    }
+}
+
