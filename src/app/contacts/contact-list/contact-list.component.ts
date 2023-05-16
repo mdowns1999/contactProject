aadbf2e2a4aca7df65b5f12678a9ca626a01b842
mdowns <michaeljdowns99@gmail.com>
2023-05-16 10:14:15 -0600

    Finished Documents Service

--- src/app/contacts/contact-list/contact-list.component.ts
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Contact } from '../contact.model';
+import { ContactService } from '../contact.service';
 
 @Component({
   selector: 'cms-contact-list',
@@ -8,16 +9,22 @@ import { Contact } from '../contact.model';
 })
 export class ContactListComponent {
 
-  @Output() selectedContactEvent = new EventEmitter<Contact>();
+  
 
+  // contacts: Contact[] = [
+  //   new Contact("1", "R. Kent Jackson","jacksonk@byui.edu", "208-496-3771", "../../assets/images/jacksonk.jpg", null),
+  //   new Contact("2", "Rex Barzee", "barzeer@byui.edu", "208-496-3771", "../../assets/images/barzeer.jpg", null)
+  // ];
+  contacts: Contact[] = [];
 
-  contacts: Contact[] = [
-    new Contact("1", "R. Kent Jackson","jacksonk@byui.edu", "208-496-3771", "../../assets/images/jacksonk.jpg", null),
-    new Contact("2", "Rex Barzee", "barzeer@byui.edu", "208-496-3771", "../../assets/images/barzeer.jpg", null)
-  ];
+  constructor(private contactService: ContactService){}
+
+  ngOnInit(){
+    this.contacts = this.contactService.getContacts();
+  }
 
   onSelected(contact: Contact){
-    this.selectedContactEvent.emit(contact);
+    this.contactService.contactSelectedEvent.emit(contact);
   }
     
 }
--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { Contact } from './contact.model';
+import { ContactService } from './contact.service';
 
 @Component({
   selector: 'cms-contacts',
   templateUrl: './contacts.component.html',
-  styleUrls: ['./contacts.component.css']
+  styleUrls: ['./contacts.component.css'],
+  providers: [ContactService]
 })
 export class ContactsComponent {
 
+  constructor(private contactService: ContactService){}
+
   selectedContact: Contact;
+
+
+  ngOnInit(){
+    this.contactService.contactSelectedEvent.subscribe(
+      (contact: Contact) => {this.selectedContact = contact}
+    )
+  }
 }
